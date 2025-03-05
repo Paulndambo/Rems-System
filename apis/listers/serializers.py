4292d85a25b2fc95db38cc0a5736a574cc6bae50
Paul Ndambo <52633424+Paulndambo@users.noreply.github.com>
2025-03-05 19:00:43 +0300

    Create serializers.py

---
+++ apis/listers/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from website.models import Lister
+
+class ListerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lister
+        fields = "__all__"
